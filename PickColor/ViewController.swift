d9a43a87337636028f3013e494b33918120b9119
MaDoG <aliasgraphics@icloud.com>
2025-02-02 16:08:09 +0300

    Initial Commit

---
+++ PickColor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PickColor
+//
+//  Created by Denis Lachikhin on 02.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

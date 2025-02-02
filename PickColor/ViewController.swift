f5e79d1702005ab0a55d5e23c14fc866ee3c468d
MaDoG <aliasgraphics@icloud.com>
2025-02-02 20:31:31 +0300

    ViewController organization

--- PickColor/ViewController.swift
+++ PickColor/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 final class ViewController: UIViewController {
-
+//    MARK: - IB Outlets
     @IBOutlet private var colorView: UIView!
 
     @IBOutlet private var redSlider: UISlider!
@@ -19,35 +19,10 @@ final class ViewController: UIViewController {
     @IBOutlet private var greenValueLabel: UILabel!
     @IBOutlet private var blueValueLabel: UILabel!
     
-    var redSliderValue = ""
-    
+//    MARK: - View Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupColorView()
-        setupColorLabels()
-    }
-    
-    @IBAction private func redSliderAction() {
-        redValueLabel.text = String(format: "%.2f", redSlider.value)
-        colorView.backgroundColor = .init(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
-    }
-    @IBAction private func greenSliderAction() {
-        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
-        colorView.backgroundColor = .init(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
-    }
-    @IBAction private func blueSliderAction() {
-        blueValueLabel.text = String(format: "%.1f", blueSlider.value)
-        colorView.backgroundColor = .init(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
-    }
-    
-    //MARK: - Setup UI
-    private func setupColorLabels() {
-        redValueLabel.text = String(redSlider.value)
-        greenValueLabel.text = String(greenSlider.value)
-        blueValueLabel.text = String(blueSlider.value)
-    }
-    
-    private func setupColorView() {
+        
         colorView.layer.cornerRadius = 10
         
         colorView.backgroundColor = .init(
@@ -56,6 +31,29 @@ final class ViewController: UIViewController {
             blue: CGFloat(blueSlider.value),
             alpha: 1.0
         )
+        
+        redValueLabel.text = String(redSlider.value)
+        greenValueLabel.text = String(greenSlider.value)
+        blueValueLabel.text = String(blueSlider.value)
+    }
+    
+//    MARK: - IB Actions
+    @IBAction private func redSliderAction() {
+        redValueLabel.text = String(format: "%.2f", redSlider.value)
+        colorViewChange()
+    }
+    @IBAction private func greenSliderAction() {
+        greenValueLabel.text = String(format: "%.2f", greenSlider.value)
+        colorViewChange()
+    }
+    @IBAction private func blueSliderAction() {
+        blueValueLabel.text = String(format: "%.2f", blueSlider.value)
+        colorViewChange()
+    }
+    
+//    MARK: - ColorViewChange method
+    private func colorViewChange() {
+        colorView.backgroundColor = .init(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1.0)
     }
 }
 

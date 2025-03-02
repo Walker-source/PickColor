acd019f03e938235c01815c1e3fac62db4612b68
MaDoG <aliasgraphics@icloud.com>
2025-03-02 19:26:47 +0300

    MainViewController setup

---
+++ PickColor/ColorPickerViewController.swift
@@ -0,0 +1,65 @@
+//
+//  MainViewController.swift
+//  PickColor
+//
+//  Created by Denis Lachikhin on 17.02.2025.
+//
+
+import UIKit
+
+class ColorPickerViewController: UIViewController {
+// MARK: - IB Outlets
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+// MARK: - View Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        colorView.layer.cornerRadius = 15
+        setColor()
+        redLabel.text = string(from: redSlider)
+        greenLabel.text = string(from: greenSlider)
+        blueLabel.text = string(from: blueSlider)
+    }
+    
+// MARK: - IB Actions
+    @IBAction private func pickColor(_ sender: UISlider) {
+        setColor()
+        
+        switch sender {
+        case redSlider:
+            redLabel.text = string(from: redSlider)
+        case greenSlider:
+            greenLabel.text = string(from: greenSlider)
+        default:
+            blueLabel.text = string(from: blueSlider)
+        }
+    }
+   
+// MARK: - Private Methods
+    private func setColor() {
+        colorView.backgroundColor = UIColor(
+            red: redSlider.value.cgFloat(),
+            green: greenSlider.value.cgFloat(),
+            blue: blueSlider.value.cgFloat(),
+            alpha: 1
+        )
+    }
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
+}
+
+extension Float {
+    func cgFloat() -> CGFloat {
+        CGFloat(self)
+    }
+}
--- PickColor/MainViewController.swift
+++ PickColor/MainViewController.swift
@@ -2,64 +2,41 @@
 //  MainViewController.swift
 //  PickColor
 //
-//  Created by Denis Lachikhin on 17.02.2025.
+//  Created by Denis Lachikhin on 02.03.2025.
 //
 
 import UIKit
 
-class MainViewController: UIViewController {
-// MARK: - IB Outlets
-    @IBOutlet var colorView: UIView!
+
+/// Берём цвет с UIView
+protocol ColorPickerVCDelegate: AnyObject {
     
-    @IBOutlet var redLabel: UILabel!
-    @IBOutlet var greenLabel: UILabel!
-    @IBOutlet var blueLabel: UILabel!
-    
-    @IBOutlet var redSlider: UISlider!
-    @IBOutlet var greenSlider: UISlider!
-    @IBOutlet var blueSlider: UISlider!
-    
-// MARK: - View Life Cycles
+    /// Присваиваем экрану цвет с экрана настроек
+    /// - Parameter color: Цвет экрана настроек
+    func getViewColor(_ color: UIView)
+}
+
+final class MainViewController: UIViewController {
+    var viewColor = UIColor.white
+    // MARK: - View Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        colorView.layer.cornerRadius = 15
-        setColor()
-        redLabel.text = string(from: redSlider)
-        greenLabel.text = string(from: greenSlider)
-        blueLabel.text = string(from: blueSlider)
+        view.backgroundColor = viewColor
     }
     
-// MARK: - IB Actions
-    @IBAction private func pickColor(_ sender: UISlider) {
-        setColor()
-        
-        switch sender {
-        case redSlider:
-            redLabel.text = string(from: redSlider)
-        case greenSlider:
-            greenLabel.text = string(from: greenSlider)
-        default:
-            blueLabel.text = string(from: blueSlider)
-        }
-    }
-   
-// MARK: - Private Methods
-    private func setColor() {
-        colorView.backgroundColor = UIColor(
-            red: redSlider.value.cgFloat(),
-            green: greenSlider.value.cgFloat(),
-            blue: blueSlider.value.cgFloat(),
-            alpha: 1
-        )
-    }
-    private func string(from slider: UISlider) -> String {
-        String(format: "%.2f", slider.value)
+// MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let colorPickerVC = segue.destination as? ColorPickerViewController
+        colorPickerVC?.viewColor = viewColor
+        colorPickerVC?.delegate = self
     }
 }
 
-extension Float {
-    func cgFloat() -> CGFloat {
-        CGFloat(self)
+// MARK: - Main View Controller
+extension MainViewController: ColorPickerVCDelegate {
+    func getViewColor(_ color: UIView ) {
+        view.backgroundColor = color.backgroundColor
+        viewColor = color.backgroundColor ?? UIColor.white
     }
 }
